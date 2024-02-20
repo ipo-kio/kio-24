662288abd13350ae8b2ae48f01cc98ed46c2632c
Eugene Polevikov <eugene.polevikov@yandex.ru>
2024-02-20 11:03:34 +0300

    Temporarly remove new tasks sources

--- tasks/proteinfolding/constants/FoldingStage.js
+++
@@ -1,2 +0,0 @@
-export const STAGE_WIDTH = 1440; //window.innerWidth * 0.75;
-export const STAGE_HEIGHT = STAGE_WIDTH * 0.525;
--- tasks/proteinfolding/constants/MovementControl.js
+++
@@ -1,9 +0,0 @@
-export const MOVEMENT_DIRECTIONS = [
-  { 'key': 0, 'arrow': '\u21E6' }, // left
-  { 'key': 1, 'arrow': '\u21E8' }, // right
-  { 'key': 2, 'arrow': '\u21E7' }, // up
-  { 'key': 3, 'arrow': '\u21E9' }  // down
-];
-
-export const MOVE_DISTANCE = 3.0;
-export const MOVE_TIMEOUT = 10;

729caf40aff9f72cb6a34e3d5a3c11b99afd9bcc
Eugene Polevikov <eugene.polevikov@yandex.ru>
2024-02-24 11:32:01 +0300

    Update UI for protein folding task

--- tasks/proteinfolding/components/ParticlesChain/ParticlesChain.js
+++ tasks/proteinfolding/components/ParticlesChain/ParticlesChain.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Circle, Line } from 'react-konva';
 
 import {
-  PARTICLE_COLORS
+  PARTICLE_COLORS,
+  PARTICLE_SHADOW_MIDDLE,
+  PARTICLE_SHADOW_SMALL
 } from '../../constants/ParticlesChain';
 
 function joinLinePoints(particles) {
@@ -58,19 +60,41 @@ export function ParticlesChain({
     setPivotParticleId(Number(particleId));
   }
 
-  const particlesChain = particles.map((particle) => (
-    <Circle
-      key={particle.id}
-      id={particle.id}
-      x={particle.x}
-      y={particle.y}
-      radius={particleRadius}
-      fill={PARTICLE_COLORS[particle.color]}
-      stroke={'black'}
-      strokeWidth={particle.id == pivotParticleId ? 5 : 2}
-      onClick={() => handleParticleClick(particle.id)}
-    />)
-  );
+  const particlesChain =
+    <>
+      {particles.map((particle) => (
+        <Circle
+          key={particle.id}
+          id={particle.id}
+          x={particle.x}
+          y={particle.y}
+          radius={particleRadius}
+          fill={PARTICLE_COLORS[particle.color]}
+          stroke={'black'}
+          strokeWidth={particle.id == pivotParticleId ? 5 : 2}
+          onClick={() => handleParticleClick(particle.id)}
+      />))}
+      {particles.map((particle) => (
+        <Circle
+          key={particle.id}
+          id={particle.id}
+          x={particle.x + particleRadius * 0.15}
+          y={particle.y - particleRadius * 0.15}
+          radius={particleRadius * 0.75}
+          fill={PARTICLE_SHADOW_MIDDLE[particle.color]}
+          onClick={() => handleParticleClick(particle.id)}
+      />))}
+      {particles.map((particle) => (
+        <Circle
+          key={particle.id}
+          id={particle.id}
+          x={particle.x + particleRadius * 0.45}
+          y={particle.y - particleRadius * 0.45}
+          radius={particleRadius * 0.2}
+          fill={PARTICLE_SHADOW_SMALL[particle.color]}
+          onClick={() => handleParticleClick(particle.id)}
+      />))}
+    </>
 
   let joinLine;
 
--- tasks/proteinfolding/constants/ParticlesChain.js
+++ tasks/proteinfolding/constants/ParticlesChain.js
@@ -1,1 +1,4 @@
-export const PARTICLE_COLORS = {red: '#FFA9E7', green: '#BFDE00', blue: '#2FD9FF'};
+export const PARTICLE_COLORS = {red: '#FF87DD', green: '#BFDE00', blue: '#2FD9FF'};
+export const PARTICLE_SHADOW_MIDDLE = {red: '#FFA9E7', green: '#CCED00', blue: '#83E8FF'};
+export const PARTICLE_SHADOW_SMALL = {red: '#FFD6F3', green: '#ECFF7A', blue: '#D2F7FF'};
+
